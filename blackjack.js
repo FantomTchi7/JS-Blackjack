9ef3a6de103378daa5c4d8cc43bdf3b110c2cfaf
FantomTchi7 <fantomcorp@outlook.com>
2025-02-02 18:09:31 +0200

    JS Blackjack initial commit

--- blackjack.js
+++ blackjack.js
@@ -1,148 +1,196 @@
+class BlackjackGame {
+    constructor() {
+        this.deck = [];
+        this.dealerHand = [];
+        this.playerHand = [];
+        this.gameInProgress = false;
+        this.cardWidth = 169.075;
+        this.cardHeight = 244.640;
+        
+        // Initialize UI elements
+        this.dealButton = document.getElementById('deal-button');
+        this.hitButton = document.getElementById('hit-button');
+        this.standButton = document.getElementById('stand-button');
+        this.dealerCardsContainer = document.getElementById('dealer-cards');
+        this.playerCardsContainer = document.getElementById('player-cards');
+        this.messageElement = document.getElementById('message');
+        this.dealerSum = document.getElementById('dealer-sum');
+        this.playerSum = document.getElementById('player-sum');
 
-let dealerSum = 0;
-let yourSum = 0;
+        // Bind event handlers
+        this.dealButton.addEventListener('click', () => this.startNewGame());
+        this.hitButton.addEventListener('click', () => this.handleHit());
+        this.standButton.addEventListener('click', () => this.handleStand());
+    }
 
-let dealerAceCount = 0;
-let yourAceCount = 0; 
-
-let hidden;
-let deck;
-
-let canHit = true; //allows the player (you) to draw while yourSum <= 21
-
-window.onload = function() {
-    buildDeck();
-    shuffleDeck();
-    startGame();
-}
-
-function buildDeck() {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let types = ["C", "D", "H", "S"];
-    deck = [];
-
-    for (let i = 0; i < types.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + "-" + types[i]); //A-C -> K-C, A-D -> K-D
+    initializeDeck() {
+        const suits = ['heart', 'spade', 'diamond', 'club'];
+        const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+        
+        this.deck = [];
+        for (const suit of suits) {
+            for (const value of values) {
+                this.deck.push({ suit, value });
+            }
         }
     }
-    // console.log(deck);
-}
 
-function shuffleDeck() {
-    for (let i = 0; i < deck.length; i++) {
-        let j = Math.floor(Math.random() * deck.length); // (0-1) * 52 => (0-51.9999)
-        let temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-    console.log(deck);
-}
-
-function startGame() {
-    hidden = deck.pop();
-    dealerSum += getValue(hidden);
-    dealerAceCount += checkAce(hidden);
-    // console.log(hidden);
-    // console.log(dealerSum);
-    while (dealerSum < 17) {
-        //<img src="./cards/4-C.png">
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";
-        dealerSum += getValue(card);
-        dealerAceCount += checkAce(card);
-        document.getElementById("dealer-cards").append(cardImg);
-    }
-    console.log(dealerSum);
-
-    for (let i = 0; i < 2; i++) {
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";
-        yourSum += getValue(card);
-        yourAceCount += checkAce(card);
-        document.getElementById("your-cards").append(cardImg);
+    shuffleDeck() {
+        for (let i = this.deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+        }
     }
 
-    console.log(yourSum);
-    document.getElementById("hit").addEventListener("click", hit);
-    document.getElementById("stay").addEventListener("click", stay);
-
-}
-
-function hit() {
-    if (!canHit) {
-        return;
-    }
-
-    let cardImg = document.createElement("img");
-    let card = deck.pop();
-    cardImg.src = "./cards/" + card + ".png";
-    yourSum += getValue(card);
-    yourAceCount += checkAce(card);
-    document.getElementById("your-cards").append(cardImg);
-
-    if (reduceAce(yourSum, yourAceCount) > 21) { //A, J, 8 -> 1 + 10 + 8
-        canHit = false;
-    }
-
-}
-
-function stay() {
-    dealerSum = reduceAce(dealerSum, dealerAceCount);
-    yourSum = reduceAce(yourSum, yourAceCount);
-
-    canHit = false;
-    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
-
-    let message = "";
-    if (yourSum > 21) {
-        message = "You Lose!";
-    }
-    else if (dealerSum > 21) {
-        message = "You win!";
-    }
-    //both you and dealer <= 21
-    else if (yourSum == dealerSum) {
-        message = "Tie!";
-    }
-    else if (yourSum > dealerSum) {
-        message = "You Win!";
-    }
-    else if (yourSum < dealerSum) {
-        message = "You Lose!";
-    }
-
-    document.getElementById("dealer-sum").innerText = dealerSum;
-    document.getElementById("your-sum").innerText = yourSum;
-    document.getElementById("results").innerText = message;
-}
-
-function getValue(card) {
-    let data = card.split("-"); // "4-C" -> ["4", "C"]
-    let value = data[0];
-
-    if (isNaN(value)) { //A J Q K
-        if (value == "A") {
+    getCardValue(card) {
+        if (['jack', 'queen', 'king'].includes(card.value)) {
+            return 10;
+        } else if (card.value === '1') { // Ace
             return 11;
+        } else {
+            return parseInt(card.value);
         }
-        return 10;
     }
-    return parseInt(value);
+
+    calculateHandValue(hand) {
+        let value = 0;
+        let aces = 0;
+
+        for (const card of hand) {
+            if (card.value === '1') {
+                aces += 1;
+            }
+            value += this.getCardValue(card);
+        }
+
+        // Adjust for aces
+        while (value > 21 && aces > 0) {
+            value -= 10;
+            aces -= 1;
+        }
+
+        return value;
+    }
+
+    drawCard(isDealer, hidden = false) {
+        const card = this.deck.pop();
+        const hand = isDealer ? this.dealerHand : this.playerHand;
+        hand.push(card);
+
+        const container = isDealer ? this.dealerCardsContainer : this.playerCardsContainer;
+        const cardIndex = hand.length - 1;
+        const overlapFactor = 0.3;
+        const x = cardIndex * (this.cardWidth * overlapFactor);
+
+        const useElement = document.createElementNS("http://www.w3.org/2000/svg", "use");
+        useElement.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", 
+            hidden ? "./SVG-cards/svg-cards.svg#back" : `./SVG-cards/svg-cards.svg#${card.suit}_${card.value}`);
+        useElement.setAttribute("x", x);
+        
+        if (hidden) {
+            useElement.setAttribute("id", "hidden-card");
+        }
+
+        container.appendChild(useElement);
+        
+        return this.calculateHandValue(hand);
+    }
+
+    startNewGame() {
+        // Reset game state
+        this.dealerHand = [];
+        this.playerHand = [];
+        this.gameInProgress = true;
+        this.messageElement.textContent = '';
+        this.messageElement.className = 'message';
+        
+        // Clear the card containers
+        this.dealerCardsContainer.innerHTML = '';
+        this.playerCardsContainer.innerHTML = '';
+        
+        // Initialize and shuffle deck
+        this.initializeDeck();
+        this.shuffleDeck();
+        
+        // Deal initial cards
+        this.drawCard(true, true); // Dealer's hidden card
+        this.drawCard(true); // Dealer's visible card
+        this.drawCard(false); // Player's first card
+        this.drawCard(false); // Player's second card
+        
+        // Update UI
+        this.updateSums();
+        this.hitButton.disabled = false;
+        this.standButton.disabled = false;
+        this.dealButton.disabled = true;
+        
+        // Check for natural blackjack
+        if (this.calculateHandValue(this.playerHand) === 21) {
+            this.handleStand();
+        }
+    }
+
+    handleHit() {
+        const playerValue = this.drawCard(false);
+        this.updateSums();
+        
+        if (playerValue > 21) {
+            this.endGame('Player busts! Dealer wins!', 'lose');
+        }
+    }
+
+    handleStand() {
+        // Reveal dealer's hidden card
+        const hiddenCard = document.getElementById('hidden-card');
+        hiddenCard.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", 
+            `./SVG-cards/svg-cards.svg#${this.dealerHand[0].suit}_${this.dealerHand[0].value}`);
+
+        // Dealer must hit on 16 and stand on 17
+        while (this.calculateHandValue(this.dealerHand) < 17) {
+            this.drawCard(true);
+        }
+
+        this.updateSums();
+        this.determineWinner();
+    }
+
+    updateSums() {
+        const playerValue = this.calculateHandValue(this.playerHand);
+        const dealerValue = this.calculateHandValue(this.dealerHand);
+        
+        this.playerSum.textContent = playerValue;
+        // Only show the value of the visible dealer card during play
+        this.dealerSum.textContent = this.gameInProgress ? 
+            this.getCardValue(this.dealerHand[1]) : dealerValue;
+    }
+
+    determineWinner() {
+        const playerValue = this.calculateHandValue(this.playerHand);
+        const dealerValue = this.calculateHandValue(this.dealerHand);
+
+        if (dealerValue > 21) {
+            this.endGame('Dealer busts! Player wins!', 'win');
+        } else if (playerValue > dealerValue) {
+            this.endGame('Player wins!', 'win');
+        } else if (dealerValue > playerValue) {
+            this.endGame('Dealer wins!', 'lose');
+        } else {
+            this.endGame('Push - It\'s a tie!', 'tie');
+        }
+    }
+
+    endGame(message, result) {
+        this.gameInProgress = false;
+        this.messageElement.textContent = message;
+        this.messageElement.className = `message ${result}`;
+        this.hitButton.disabled = true;
+        this.standButton.disabled = true;
+        this.dealButton.disabled = false;
+        this.updateSums();
+    }
 }
 
-function checkAce(card) {
-    if (card[0] == "A") {
-        return 1;
-    }
-    return 0;
-}
-
-function reduceAce(playerSum, playerAceCount) {
-    while (playerSum > 21 && playerAceCount > 0) {
-        playerSum -= 10;
-        playerAceCount -= 1;
-    }
-    return playerSum;
-}
+// Start the game when the page loads
+window.onload = () => {
+    new BlackjackGame();
+};
